a119d981e4632dcad4c553a2426024d754fcb9cd
Augusto <senescal.oliveira@gmail.com>
2026-01-13 16:30:42 -0300

    feat: concluindo css e adicionando notas com javascript

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// Eventos
+const notesContainer = document.querySelector("#notes-container");
+
+const noteInput = document.querySelector("#note-content");
+
+const addNoteBtn = document.querySelector(".add-note");
+
+// Funções
+
+function addNote(){
+    const noteObject = {
+        id: generateId(),
+        content: noteInput.value,
+        fixed: false,
+    };
+
+    const noteElement = createNote(noteObject.id, noteObject.content);
+
+    notesContainer.appendChild(noteElement);
+}
+
+function createNote(id, content, fixed){
+    const element = document.createElement("div")
+
+    element.classList.add("note")
+
+    const textatea = document.createElement("textarea")
+
+    textatea.value = content
+
+    textatea.placeholder = "Adicione algum texto..."
+
+    element.appendChild(textatea);
+
+    return element;
+}
+
+function generateId(){
+    return Math.floor(Math.random() * 5000);
+}
+
+// Eventos
+addNoteBtn.addEventListener("click", () => {
+    addNote();
+});
