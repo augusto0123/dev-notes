8035484a4ae6357de290e371a7e6683250fc0782
Augusto <senescal.oliveira@gmail.com>
2026-01-14 08:43:12 -0300

    feat: buscar por notas

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // Eventos
 const notesContainer = document.querySelector("#notes-container");
-
 const noteInput = document.querySelector("#note-content");
-
 const addNoteBtn = document.querySelector(".add-note");
+const searchInput = document.querySelector("#search-input");
 
 // Funções
 
@@ -137,10 +136,41 @@ function saveNotes(notes){
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
+function searchNotes(search){
+    const searchResults = getNotes().filter((note) => {
+        return note.content.includes(search);
+    })
+
+    if(search !== ""){
+        cleanNotes();
+        searchResults.forEach((note) => {
+            const noteElement = createNote(note.id, note.content)
+            notesContainer.appendChild(noteElement);
+        });
+
+        return;
+    }
+
+    cleanNotes();
+
+    showNotes();
+}
+
 // Eventos
 addNoteBtn.addEventListener("click", () => {
     addNote();
 });
+
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+    searchNotes(search);
+});
+
+noteInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        addNote();
+    }
+})
 
 // Inicializacao
 showNotes();
